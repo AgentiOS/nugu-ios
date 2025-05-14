9a8cd9e372bf6180c79b6b658293071b6f36d029
이상근님/AI Assistant iOS팀 <sangkeun.lee@sk.com>
2025-05-14 19:57:56 +0900

    PlaySyncManager Combine으로 변경

--- NuguCoreTests/NuguCoreTests.swift
+++ NuguCoreTests/NuguCoreTests.swift
@@ -9,6 +9,7 @@
 import Testing
 
 @testable import NuguCore
+@testable import NuguUtils
 
 struct NuguCoreTests {
     @Test("get contexts using combine")
@@ -38,5 +39,90 @@ struct NuguCoreTests {
                 completion(.init(contextType: .client, name: namespace, payload: [:] as [String: AnyHashable]))
             }
         }
+    }
+    
+    @Test("playSyncManager startPlay")
+    func playSyncManagerTest1() async throws {
+        let contextManager = ContextManager()
+        let sut = PlaySyncManager(contextManager: contextManager)
+        
+        let playStackServiceId = "playStackServiceId"
+        let dialogRequestId = "dialogRequestId"
+        let messageId = "messageId"
+        sut.startPlay(
+            property: PlaySyncProperty(
+                layerType: .media,
+                contextType: .display
+            ),
+            info: PlaySyncInfo(
+                playStackServiceId: playStackServiceId,
+                dialogRequestId: dialogRequestId,
+                messageId: messageId,
+                duration: NuguTimeInterval(seconds: 3)
+            )
+        )
+        
+        let result = await withCheckedContinuation { continuation in
+            NotificationCenter.default.addObserver(
+                forName: Notification.Name.playSyncPropertiesDidChange,
+                object: nil,
+                queue: nil
+            ) { notification in
+                let result = (notification.userInfo?["properties"] as? [(property: PlaySyncProperty, info: PlaySyncInfo)])?.first
+                if let property = result?.property,
+                   property.layerType == .media,
+                   property.contextType == .display,
+                    let info = result?.info,
+                   info.playStackServiceId == playStackServiceId,
+                   info.dialogRequestId == dialogRequestId,
+                   info.messageId == messageId {
+                    continuation.resume(returning: true)
+                }
+            }
+        }
+        
+        #expect(result == true)
+    }
+    
+    @Test("playSyncManager startPlay")
+    func playSyncManagerTest2() async throws {
+        let contextManager = ContextManager()
+        let sut = PlaySyncManager(contextManager: contextManager)
+        
+        let property = PlaySyncProperty(
+            layerType: .media,
+            contextType: .display
+        )
+        let playStackServiceId = "playStackServiceId"
+        let dialogRequestId = "dialogRequestId"
+        let messageId = "messageId"
+        sut.startPlay(
+            property: property,
+            info: PlaySyncInfo(
+                playStackServiceId: playStackServiceId,
+                dialogRequestId: dialogRequestId,
+                messageId: messageId,
+                duration: NuguTimeInterval(seconds: 3)
+            )
+        )
+        
+        sut.endPlay(property: property)
+        
+        let result = await withCheckedContinuation { continuation in
+            NotificationCenter.default.addObserver(
+                forName: Notification.Name.playSyncPropertyDidRelease,
+                object: nil,
+                queue: nil
+            ) { notification in
+                if let property = notification.userInfo?["property"] as? PlaySyncProperty,
+                   property.layerType == .media,
+                   property.contextType == .display,
+                   (notification.userInfo?["messageId"] as? String) == messageId {
+                    continuation.resume(returning: true)
+                }
+            }
+        }
+        
+        #expect(result == true)
     }
 }
