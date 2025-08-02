8aee2060c322f655fc26cb14e845d622b83864b6
jayce1116 <jayceSub@gmail.com>
2025-08-02 23:20:52 +0900

    Fix PlaySyncManager (#201)
    * add playSyncInfo
    
    * fix playSyncMangager timer logics
    
    * revert playSyncInfo codes
    
    ---------
    
    Co-authored-by: jaycesub <jsshin1116@sktelecom.com>

--- NuguCore/Sources/PlaySync/PlaySyncManageable.swift
+++ NuguCore/Sources/PlaySync/PlaySyncManageable.swift
@@ -24,10 +24,7 @@ import NuguUtils
 
 /// <#Description#>
 public protocol PlaySyncManageable: ContextInfoProvidable, TypedNotifyable {
-    func startPlay(
-        property: PlaySyncProperty,
-        info: PlaySyncInfo
-    )
+    func startPlay(property: PlaySyncProperty, info: PlaySyncInfo)
     
     /// <#Description#>
     /// - Parameter property: <#property description#>
