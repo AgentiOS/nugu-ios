96f92dd996e2f3b49746e0a0cc0996308046430d
jayce1116 <jayceSub@gmail.com>
2025-08-07 14:31:52 +0900

    fix audio player should resume logic (#212)
    Co-authored-by: jaycesub <jsshin1116@sktelecom.com>

--- NuguAgents/Sources/CapabilityAgents/AudioPlayer/AudioPlayerAgent.swift
+++ NuguAgents/Sources/CapabilityAgents/AudioPlayer/AudioPlayerAgent.swift
@@ -558,7 +558,7 @@ private extension AudioPlayerAgent {
                 if let currentPlayer = self.currentPlayer,
                    player.shouldResume(player: currentPlayer) {
                     log.info("Resuming \(player.header.messageId)")
-                    currentPlayer.stop()
+                    currentPlayer.pause()
                     player.replacePlayer(currentPlayer)
                 }
                 self.currentPlayer = player
